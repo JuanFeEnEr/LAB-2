bc5ab3178c6f63486e399d2315e0cb92e2df4a45
Juan Enriquez <jfenriqueze@gmail.com>
2024-11-08 14:23:30 -0500

    Primer cambio

---
+++ src/chatbot/Chatbot.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatbot;
+
+/**
+ *
+ * @author USER
+ */
+public class Chatbot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Chatframe frame = new Chatframe();
+        frame.setVisible(true);
+        frame.setSize(600,600);
+        
+    }
+    
+}
